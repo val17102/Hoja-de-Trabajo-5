f9cb593090416f293e32aeaa00973003a41d0212
val17102 <32184574+val17102@users.noreply.github.com>
2018-03-01 22:09:24 -0600

    Add files via upload

--- SO.py
+++ SO.py
@@ -1,97 +1,92 @@
 import simpy
-import math
 import random
+import math
 
-
+random.seed(10)
 
 mram = 100
 
 instrucciones = 3
 
-tiempos = [] 
+operacion = 5
 
-capacidad_procesos = 2
+tiempos = []
+
+capacidad_proceso = 2
 
 intervalo = 10
 
 cantidad_procesos = 25
 
-env = simpy.Environment()
-
-ram = simpy.Container(env, init = mram, capacity = mram)
-
-cpu = simpy.Resource(env, capacity = capacidad_procesos)
 
 class SO:
-    def __init__(self):
-        self.poop = 0
-
+        def __init__ (self, env):
+                self.ram = simpy.Container(env, init=mram, capacity=mram)
+                self.cpu = simpy.Resource(env, capacity=capacidad_proceso)
 
 class Proceso:
-    def __init__(self, nom, num, env, so):
+    def __init__(self, nom, num,env, so):
         self.env = env
         self.terminado = False
         self.nombre = nom
-        self.memoriaR = random.randint(1,10)
         self.numInstrucciones = random.randint(1,10)
-        self.tiempo_crear = 0
-        self.tiempo_fin = 0
-        self.tiempo_total = 0
-        self.numero = num
+        self.memoriaR = random.randint(1,10) 
+        self.tiempo_crear = 0 
+        self.tiempo_fin = 0 
+        self.tiempo_total = 0 
+        self.numero = num 
         self.sistema_operativo = so
         self.proceso = env.process(self.procesar(env, so))
 
-    def procesar(self, env):
+    def procesar(self,env,so):
+        inicio = env.now
         self.tiempo_crear = env.now
-        print('%s: creado a las %d',(self.nombre, self.tiempo_crear))
-        with ram.get(self.memRequerida) as getRam:
-            yield getRam
-            print('%s: esta en la Ram a las %d' % (self.nombre, env.now))
-            siguiente = 0
-            while not self.terminado:
-                with cpu.request() as req:
-                    print('%s: quiere entrar al CPU a las %d' %(self.nombre, env.now))
-                    yield req
-                    print('%s: entra al CPU a las %d' %(self.nombre, env.now))
-                    for i in range(instrucciones):
-                        if self.numInstrucciones > 0:
-                            self.numInstrucciones = self.numInstrucciones - 1
-                            siguiente = random.randint(0,1)
-                    yield env.timeout(1)
-    
-                    if siguiente == 0:
-                        yield env.timeout(5)
-                    if self.numInstrucciones == 0:
-                        self.terminado = True
-            print('%s: Termina a %d' %(self.nombre,env.now))
-            ram.put(self.memoriaR)
+        print ('%s: creado a las %d.'%(self.nombre, inicio))
+        with so.ram.get(self.memoriaR) as getRam:  
+                yield getRam
+                print('%s: esta en la Ram a las %d.' % (self.nombre, env.now)) 
+                siguiente = 0 
+                
+                while not self.terminado: 
+                        with so.cpu.request() as req:
+                                print('%s: quiere entrar al CPU a las %d.' % (self.nombre, env.now))
+                                yield req
+                                print('%s: entra al CPU a las %d.' % (self.nombre, env.now))
+                                for i in range (instrucciones):  
+                                        if self.numInstrucciones > 0:
+                                                self.numInstrucciones = self.numInstrucciones - 1
+                                                siguiente = random.randint(0,2)  
+                                yield env.timeout(1)  
+                                if siguiente == 0:
+                                        yield env.timeout(operacion)
+                                if self.numInstrucciones ==1:
+                                        self.terminado = True
+                print('%s: terminado a las %d' % (self.nombre, env.now))
+                so.ram.put(self.memoriaR) 
         self.tiempo_fin = env.now
-        self.timepo_total = int(self.tiempo_fin - self.tiempo_crear)
-        tiempos.insert(self.numero, self.tiempo_total)
+        self.tiempo_total = int(self.tiempo_fin - self.tiempo_crear) 
+        tiempos.insert(self.numero, self.tiempo_total) 
 
-def crear_proceso(env, i):
+
+def crear_procesos(env, so,i):
         tiempo_crear = random.expovariate(1.0/intervalo)
-        Proceso('Proceso %d' % i, i, env)
-        yield env.timeout(tiempo_crear)
-
-        
-class Main(object):
-    def _init_(self):
-        sistema_operativo = SO()
+        Proceso('Proceso # %d' % i, i, env, so)
+        yield env.timeout(tiempo_crear)  
+    
+class principal(object):     
+    def __init__(self):
+        env = simpy.Environment()  
+        sistema_operativo = SO(env)  
         for i in range (cantidad_procesos):
-            env.process(crear_proceso(env, i))
-        env.run()
-        tiempoPromedio = (sum(tiempos)*1.0/(len(tiempos))
-        print ("Promedio: ", tiempoPromedio)
+                env.process(crear_procesos(env, sistema_operativo,i))  
+        env.run()       
 
-Main()
+        tiempoTotalPromedio = sum(tiempos)*1.0/(len(tiempos))
 
-
-
-
-        
+        print ("El tiempo Promedio es de: ", tiempoTotalPromedio)
 
 
 
 
+principal()
 
